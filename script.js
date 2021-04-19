7f315da3f0a507caf046e35dd1a32d47b2398495
andres-boltdasarini <lau47@mail.ru>
2021-04-19 08:59:42 +0300

    return array selectors

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function getDomNodesBySelector(selector) {
 
+/*   let del=[]
+    for (let i = 0; i < document.querySelectorAll(selector).length; i++) {
+        del.push(document.querySelectorAll(selector)[i])
+    }
+    return del*/
+    return Array.from(document.querySelectorAll(selector))
 }
 
 console.log(getDomNodesBySelector('.price-value'));
